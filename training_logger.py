e7540b307398cb31e059d1946de75c5fb913bdcc
Ilker Karadag <decodeanddesign@gmail.com>
2026-01-27 11:25:58 -0500

    Update training_logger.py

--- training_logger.py
+++ training_logger.py
@@ -83,7 +83,7 @@ class TrainingLogger:
         self.csv_file = open(self.metrics_csv, 'w', newline='')
         self.csv_writer = csv.writer(self.csv_file)
         self.fieldnames = [
-            'epoch', 'total_loss', 'mse_loss', 'gradient_loss', 'spectral_loss', 'peak_loss',
+            'epoch', 'total_loss', 'mse_loss', 'gradient_loss', 'spectral_loss', 'peak_loss', 'wake_loss',
             'learning_rate', 'epoch_time_sec', 'best_loss', 'patience_counter'
         ]
         self.csv_writer.writerow(self.fieldnames)
