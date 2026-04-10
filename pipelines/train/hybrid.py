580072255a4d6b0b9e3bf884b646ef77f49aecdd
Thach <athach7@gatech.edu>
2026-04-09 23:06:35 -0400

    remove hardcoded usernames, use dynamic USER env var for portability

--- pipelines/train/hybrid.py
+++ pipelines/train/hybrid.py
@@ -102,10 +102,11 @@ def main():
             elif check_path(linux_path, "Linux Path"):
                 DATA_FOLDER = linux_path
             else:
+                _user = os.environ.get('USER', 'unknown')
                 guesses = [
-                    "/home/hice1/athach7/scratch/Training_Dataset",
+                    f"/home/hice1/{_user}/scratch/Training_Dataset",
+                    f"/storage/ice1/2/4/{_user}/Training_Dataset",
                     "/storage/ice1/2/4/scratch/Training_Dataset",
-                    "/storage/ice1/2/4/athach7/Training_Dataset",
                 ]
                 for guess in guesses:
                     if check_path(guess, "Hardcoded Guess"):
